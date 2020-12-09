08c8d8c18d3e50ceca85146efaf7e232329797d5
Piotrek1324 <piotrekwojcikowski2004@wp.pl>
2020-12-09 23:57:57 +0100

    added project

---
+++ Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+
+        }
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+}
